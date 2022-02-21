0cdc9112691c99d55fe1237bd4451998e5a419a4
Silvio Neto <93719621+osilvioneto@users.noreply.github.com>
2022-02-21 09:48:31 -0300

    Add files via upload

---
+++ card.js
@@ -0,0 +1,18 @@
+function img1() {
+    document.getElementById("produtofoto").src="img/oculosazul.png";
+}
+function img2() {
+    document.getElementById("produtofoto").src="img/oculosvermelho.png";
+}
+function img3() {
+    document.getElementById("produtofoto").src="img/oculosverde.png";
+}
+function img4() {
+    document.getElementById("produtofoto2").src="img/bermudaanimalprint.png";
+}
+function img5() {
+    document.getElementById("produtofoto2").src="img/bermudaanimalprint2.png";
+}
+function img6() {
+    document.getElementById("produtofoto2").src="img/bermudaanimalprint3.png";
+}
